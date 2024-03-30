535018400307c9f9286905689154d83c914ca904
Jorocel <jjorocel@gmail.com>
2024-03-30 12:32:06 +0800

    Initial commit

---
+++ OOP Assignment #2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP_Assignment__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 100; x++)
+            {
+                if (x % 15 == 0)
+                {
+                    Console.WriteLine("Hello, Goodbye!");
+                }
+                else if (x % 3 == 0)
+                {
+                    Console.WriteLine("Hello!");
+                }
+                else if (x % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
